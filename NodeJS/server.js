35cecdb3a3a073440b27f0039472a13bcc1f4043
max-lambert <33155378+max-lambert@users.noreply.github.com>
2020-03-11 02:36:45 +0100

    Add files via upload

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -3,10 +3,21 @@ const express = require('express');
 const server = express();
 
 server.get('/',function(req,res){
-  res.send('Hello Wolrd');
-
+  res.setHeader('Content-Type','text/plain');
+  res.send('Vous êtes à l\'acceuil');
 });
-
+server.get('/register',function(req,res){
+  res.setHeader('Content-Type','text/plain');
+  res.send('Vous êtes sur le point de vous enregister');
+});
+server.get('/profile',function(req,res){
+  res.setHeader('Content-Type','text/plain');
+  res.send('Vous êtes sur votre profil');
+});
+server.get('/',function(req,res){
+  res.setHeader('Content-Type','text/plain');
+  res.status(404).send('Page introuvable !');
+});
 server.listen(3000, function(){
   console.log('Server Started on port 3000 ...');
 })
